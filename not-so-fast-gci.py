5cff8c64ce6f3223269ff242f90f35440110308d
akhay_tolwani <akhaytolwanipython@gmail.com>
2020-01-15 22:28:54 +0530

    Commit

--- not-so-fast-gci.py
+++ not-so-fast-gci.py
@@ -14,5 +14,5 @@ if n > 0 and n <= 1024:
     answer = pow(2,n)
     print(answer)
 else:
-    print("Please provide a number which is more than 0 and less than oreqqual to 1024")
+    print("Please provide a number which is more than 0 and less than or equal to 1024")
     exit
